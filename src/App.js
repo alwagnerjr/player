ede04c6404b6d99e6b2d5cd8901424c060c34d88
alwagner <shelenokasya@gmail.com>
2020-09-28 16:45:46 +1000

    Перенесен код из примера в проект

--- src/App.js
+++ src/App.js
@@ -2,25 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function PlayButton(props) {
+  const className = props.isMusicPlaying ? 'play active' : 'play';
+  return <a onClick={props.onClick} href="#" title="Play video" className={className} />;
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isMusicPlaying: false };
+  }
+  handleClick() {
+    if (this.state.isMusicPlaying) {
+      this.audio.pause();
+    } else {
+      this.audio.play();
+    }
+    this.setState(prevState => {
+      return {
+        isMusicPlaying: !prevState.isMusicPlaying
+      };
+    });
+  };
+  render() {
+    return (
+        <div>
+          <PlayButton
+              onClick={this.handleClick.bind(this)}
+              isMusicPlaying={this.state.isMusicPlaying}
+          />
+          <audio id="audio" ref={(audioTag) => { this.audio = audioTag }} />
+        </div>
+    );
+  }
 }
 
 export default App;
